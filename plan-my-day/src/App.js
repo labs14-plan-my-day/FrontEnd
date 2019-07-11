23dbc9f329b97b0d1376cb311699ae44f4edd02e
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-07-11 19:04:19 -0400

    Removed boilerplate code

--- plan-my-day/src/App.js
+++ plan-my-day/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component() {
-  
+
   constructor(){
     this.state={
       response: []
@@ -25,20 +25,7 @@ class App extends Component() {
   render() {
   return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       
       </div>
       );
     }
