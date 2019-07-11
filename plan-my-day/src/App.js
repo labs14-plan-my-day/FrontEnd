a447c85630843193022bddd2215fa2192a68cd2b
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-07-11 18:49:50 -0400

    Fixing axios

--- plan-my-day/src/App.js
+++ plan-my-day/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import axios from axios;
+import axios from 'axios';
 import './App.css';
 
 class App extends Component() {
