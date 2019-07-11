89464ecab734bf9846da23bc252e919f857eb824
Tyler Spaulding <TylerSpaulding95@gmail.com>
2019-07-11 18:26:48 -0400

    Working on axios call to backend and moving to dev for deployment

--- plan-my-day/src/App.js
+++ plan-my-day/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import axios from axios;
 import './App.css';
 
-function App() {
+class App extends Component() {
   constructor(){
     super();
     this.state={
@@ -14,31 +14,33 @@ function App() {
   componentDidMount() {
     axios.get('')
     .then((res) => {
-      console.log(res);
+      console.log(res.data);
     })
     .catch((err) => {
       console.log(err);
     })
   }
 
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      );
+    }
+  }
 
 export default App;
