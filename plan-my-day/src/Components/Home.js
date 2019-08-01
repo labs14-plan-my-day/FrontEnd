bec18a54ea8a8f2ffcdc8a527d3a1cc45c327369
tca88 <trishnaamin@gmail.com>
2019-08-01 12:38:11 -0400

    Temporarily add Tasks link to tasks page.

--- plan-my-day/src/Components/Home.js
+++ plan-my-day/src/Components/Home.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
+import Link from "@material-ui/core/Link";
 
 const Home = () => (
   <Fragment>
     <h1>HOME</h1>
+    <Link href="/tasks">Tasks</Link>
   </Fragment>
 );
 
