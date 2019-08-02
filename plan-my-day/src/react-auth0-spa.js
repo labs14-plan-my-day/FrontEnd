fec99d5ee90d1305aa7a5069f3c967fb9c7367ed
tomexar <tommyexar@gmail.com>
2019-08-02 13:49:26 -0600

    changed around axios to get the login fincton to search for existing users and add them if they are new

--- plan-my-day/src/react-auth0-spa.js
+++ plan-my-day/src/react-auth0-spa.js
@@ -40,34 +40,16 @@ export const Auth0Provider = ({
           email: user.email,
           username: user.nickname,
         }
-        //console.log(newUser)
-        // After we have set the user, make a POST request to your backend and save the USER if it doesnt exist. if exist just return the user
-        // get user if !user 
-
-        // Axios.get(`http://localhost:8080/auth/${user.email}`)
-        //   .then(res => {
-        //     console.log(`this is res${res.data}`)
-        //     return res.data
-
-        //   }).catch(err => {
-        //     if (!user) {
-        //       Axios
-        //         .post('http://localhost:8080/auth/register', newUser)
-        //         .then(res => {
-        //           console.log({res, newUser})
-        //         }).catch(err => {
-        //           console.log(err)
-        //         })
-        //     }else{console.log('error')}
-        //   })
-
+      
+        console.log(user.email)
+        //console.log(user.app_metadata.onboarded)
         Axios
-          .post('http://localhost:8080/auth/register', newUser)
-          .then(res => {
-            console.log(res)
-          }).catch(err => {
-            console.log(err)
-          })
+        .post(`http://localhost:8080/auth/a/${user.email}`, newUser)
+        .then(res =>{
+          console.log('authenticate', res.data)
+        }).catch(e =>{
+          console.log(e)
+        })
 
       }
 
