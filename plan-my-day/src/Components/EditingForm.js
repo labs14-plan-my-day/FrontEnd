5ffc7d493dfc9fcb7069906fb19aaf173c4e27bd
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-08-07 12:51:14 -0400

    update form

--- plan-my-day/src/Components/EditingForm.js
+++ plan-my-day/src/Components/EditingForm.js
@@ -6,14 +6,51 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import axios from 'axios';
 
+const whatTask = {
+	name: ''
+};
 export default class extends Component {
 
+    constructor(props) {
+            super(props);
+            this.state = {
+                tasks: [ {} ],
+                task: { name: '' },
+                editingId: null,
+                activeTask: null,
+                isEditing: false
+            };
+        }
+
+    formHandler = (event) => {
+		this.setState({
+			task: {
+				...this.state.task,
+				[event.target.name]: event.target.value
+			}
+		});
+	};
+
+    updateInfo = (event, taskID) => {
+		event.preventDefault();
+		axios
+			.put(`https://plan-my-dayapp.herokuapp.com/tasks/${taskID}`, this.state.task.name)
+			.then((res) => {
+				this.setState({
+					tasks: res.data.tasks,
+					task: { name: '', task: whatTask, isEditing: false }
+				});
+			})
+			.catch((err) => console.log(err));
+	};
 
   render() {
     const open  = this.props.open;
     return(
       <Fragment>
+          
         <Dialog
           open={open}
           onClose={this.props.handleToggle}
@@ -25,19 +62,23 @@ export default class extends Component {
               
             </DialogContentText>
             <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Task"
-              type="email"
-              fullWidth
+              onChange={this.formHandler}
+                name="name"
+                hintText="Add a Task"
+                className="AddText"
+                fullWidth={true}
             />
           </DialogContent>
           <DialogActions>
             <Button onClick={this.props.handleToggle} color="primary">
               Cancel
             </Button>
-            <Button onClick={this.props.handleToggle} color="primary">
+            <Button  
+            type="Submit"
+            label="Add Task"
+            primary={true} 
+            onClick={this.updateInfo} 
+            color="primary">
               Update
             </Button>
           </DialogActions>
